5a8e7147df69157bdd5def525f501c830ef059d1
Salomon Avila <salomon.avila2506@gmail.com>
2024-11-22 20:21:41 -0500

    Iteracion

--- Plantilla.cpp
+++ Plantilla.cpp
@@ -1,34 +1,22 @@
 #include <bits/stdc++.h>
-
-//Tipos de datos y Estructuras
+using namespace std;
 #define mod %
 #define ll long long
-//Vectores
 #define vi vector<int>
 #define vl vector<ll>
-//Stack
+#define vs vector<string>
 #define si stack<int>
-
-
-//Ciclos
+#define sl stack<ll>
+#define ss stack<string>
 #define forn(i,n) for(int i = 0; i<n; i++)
 #define forn2(n,i) for(int i = n; i>=0; i--)
-
-//Operaciones
+#define forn3(n) for(auto &i : n)
 #define msum(x,y,m)  ((x mod m) + (y mod m)) mod m
 #define mres(x,y,m)  ((x mod m) - (y mod m)) mod m
 #define mmul(x,y,m)  ((x mod m) * (y mod m)) mod m
 #define mdiv(x,n,m)  expo((x mod m),n) mod m
 #define pb push_back;
-
-//Impresiones
 #define endl "\n"
-
-//COMENTARIOS IMPORTANTES
-/*
-
-
-*/
 
 ll expo(ll b, ll e ) {
     ll ans = 1;
@@ -38,10 +26,17 @@ ll expo(ll b, ll e ) {
     return ans;
 }
 
-using namespace std;
+
+void solve() {
+
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
+    int T; cin>>T;
+    while(T--) {
+        solve();
+    }
 
 }
--- PlantillaDocumentada.cpp
+++ PlantillaDocumentada.cpp
@@ -1,18 +1,23 @@
 #include <bits/stdc++.h>
 
 //Tipos de datos y Estructuras
+
 #define mod %
 #define ll long long
+
 //Vectores
 #define vi vector<int>
 #define vl vector<ll>
+#define vs vector<string>
 //Stack
 #define si stack<int>
-
+#define sl stack<ll>
+#define ss stack<string>
 
 //Ciclos
 #define forn(i,n) for(int i = 0; i<n; i++)
 #define forn2(n,i) for(int i = n; i>=0; i--)
+#define forn3(n) for(auto &i : n)
 
 //Operaciones
 
@@ -33,13 +38,15 @@
  */
 #define mdiv(x,n,m)  expo((x mod m),n) mod m
 
-
+//Push back del vector
 #define pb push_back;
+
 
 //Impresiones
 #define endl "\n"
 
 //Funciones
+
 //Exponenciar un numero con la base y el
 ll expo(ll b, ll e ) {
     ll ans = 1;
@@ -56,11 +63,18 @@ ll expo(ll b, ll e ) {
 
 */
 
-
 using namespace std;
+
+void solve() {
+
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
+    int T; cin>>T;
+    while(T--) {
+        solve();
+    }
 
 }
